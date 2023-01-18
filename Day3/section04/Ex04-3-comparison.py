3c612b2f2c06b77e40a24b930ec5cc77b536b0c4
leejaeyeob <ljy11124@naver.com>
2023-01-18 21:51:57 +0900

    ljy11124

---
+++ Day3/section04/Ex04-3-comparison.py
@@ -0,0 +1,14 @@
+'''
+관계 연산자
+    ex) >, >=, <, <=, ==, !=
+    2개 항을 비교하여 그 결과를 논리(bool) 자료형으로 변환
+'''
+a = 15
+print('{} > 10 : {}'.format(a, a > 10))
+print('{} < 10 : {}'.format(a, a < 10))
+print('{} >= 10 : {}'.format(a, a >= 10))
+print('{} <= 10 : {}'.format(a, a <= 10))
+print('{} == 10 : {}'.format(a, a == 10))
+print('{} != 10 : {}'.format(a, a != 10))
+# != - 같지 않다
+# True or False로 나옴
